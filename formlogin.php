7d88c10a7e17f7d4e22be24c8b2e003545308efc
itapurnama06 <127809157+itapurnama06@users.noreply.github.com>
2023-04-18 12:12:59 +0800

    Add files via upload

---
+++ formlogin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+
+<form action="Login.php" method="POST">
+
+    <div>
+    User Name
+        <input type="text" name="txNAMA">
+    </div>
+
+    <div>
+        Password
+        <input type="text" name="txPASS1">
+    </div>
+
+    <div>
+    <button type="submit">Login</button>
+    <button type="submit">Registrasi</button><br>
+    </div>
+
+</body>
+</html>
---
+++ konfigurasi.php
@@ -0,0 +1,10 @@
+<?php
+   //konfirgurasi database
+   define("DBHOST","localhost");
+   define("DBUSER","root");
+   define("DBPASS","");
+   define("DBPORT","3306");
+   define("DBNAME","dbmahasiswa");
+
+   //konfirgurasi Timezone
+   date_default_timezone_set('Asia/Makassar');
